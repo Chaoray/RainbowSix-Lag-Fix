11b3a4319d8671ff5f110fdb4da9388fe593bc2a
Hino Shimizu <65358638+Chaoray@users.noreply.github.com>
2023-07-13 15:02:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Rainbow Six Lag Fix
+
+A Simple Solution to the lag problem of Rainbow Six.  
+
+After playing like 2 or 3 games, I noticed that the game froze for 5~6 seconds and that happened often.  
+Pretty annoying, I searched a reddit post that described the same thing and there was a solution:  
+Open task manager, select rainbow six, set the affinity to the last core, and set it back to all core after that.  
+
+I am lazy so I coded it into a tiny program. All you need is a click.
---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <windows.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo(&sysinfo);
+    int cpuNum = sysinfo.dwNumberOfProcessors;
+
+    HANDLE hProcess;
+    HWND hWnd = FindWindowW(NULL, L"Rainbow Six");
+
+    DWORD pid, tid;
+    tid = GetWindowThreadProcessId(hWnd, &pid);
+    if (!tid) {
+        cout << "Rainbow Six Not Found\n";
+        return 1;
+    }
+
+    hProcess = OpenProcess(PROCESS_SET_INFORMATION, FALSE, pid);
+    if (hProcess == INVALID_HANDLE_VALUE) {
+        cout << "Rainbow Six Denied Access\n";
+        return 1;
+    }
+
+    DWORD_PTR dwProcessAffinityMask = 1;
+
+    for (int i = 0; i < cpuNum - 1; i++) {
+        dwProcessAffinityMask <<= 1;
+    }
+
+    BOOL ret = SetProcessAffinityMask(hProcess, dwProcessAffinityMask);
+    if (!ret) {
+        cout << "Failed to Fix Rainbow Six\n";
+        return 1;
+    }
+
+    dwProcessAffinityMask <<= 1;
+    dwProcessAffinityMask -= 1;
+
+    SetProcessAffinityMask(hProcess, dwProcessAffinityMask);
+    if (!ret) {
+        cout << "Failed to Fix Rainbow Six\n";
+        return 1;
+    }
+
+    cout << "Great! Enjoy Non-Laggy Rainbow Six\n";
+    return 0;
+}
